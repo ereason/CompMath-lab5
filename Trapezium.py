22c627c13b9e23e177768243fa03962af5951ba1
George <george@MacBook-Pro-George.local>
2021-12-21 13:39:48 +0700

    метод трапеций готов

---
+++ Trapezium.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def h(a, b):
+    return b - a
+
+
+def calculateIntegral(f, segment, step):
+    segments = [round(i, 10) for i in np.arange(segment[0], segment[-1] + step, step)]
+    sum = 0
+    for i in range(len(segments) - 1):
+        dh = h(segments[i], segments[i + 1])
+        sum += dh* (f(segments[i]) + f(segments[i + 1])) / 2.0
+    return sum
--- main.py
+++ main.py
@@ -3,36 +3,31 @@ import scipy.integrate
 import math
 import BuildPlot
 import MidSquare
+import Trapezium
 
 #1 вариант
 #WolframAlpha: интеграл на отрезке = 1.19862
-checkValue = 1.19862
+checkValueWolframAlpha = 1.19862
 segment = [1.4,5.0]
 def f(x):
     a  = (1.0+x)*(1.0+x)
     b = math.pow(x,3)*math.sqrt(2.0+x)
     return a/b
 
+def experiment(func,segment,step,description):
+    checkValue = 1.19862
+    value = func(f, segment, step)
+    print("Отрезок: ", segment, " Шаг: ", step)
+    print("WolframAlpha: ", checkValue)
+    print(description+": ", value)
+    print("разница: ", checkValue - value)
+    print("----------------------------------")
 
-step = 0.01
-t = MidSquare.calculateIntegral(f,segment,step)
-print("Отрезок: ",segment," Шаг: ",step)
-print("WolframAlpha: ",checkValue)
-print("Метод средних прямоугольников: ",t)
-print("разница: ",checkValue - t)
 
-print("----------------------------------")
-step = 0.1
-t = MidSquare.calculateIntegral(f,segment,step)
-print("Отрезок: ",segment," Шаг: ",step)
-print("WolframAlpha: ",checkValue)
-print("Метод средних прямоугольников: ",t)
-print("разница: ",checkValue - t)
+experiment(MidSquare.calculateIntegral,segment,0.01,"Метод средних прямоугольников")
+experiment(MidSquare.calculateIntegral,segment,0.1,"Метод средних прямоугольников")
+experiment(MidSquare.calculateIntegral,segment,1,"Метод средних прямоугольников")
 
-print("----------------------------------")
-step = 1
-t = MidSquare.calculateIntegral(f,segment,step)
-print("Отрезок: ",segment," Шаг: ",step)
-print("WolframAlpha: ",checkValue)
-print("Метод средних прямоугольников: ",t)
-print("разница: ",checkValue - t)
+experiment(Trapezium.calculateIntegral,segment,0.01,"Метод Трапеций")
+experiment(Trapezium.calculateIntegral,segment,0.1,"Метод Трапеций")
+experiment(Trapezium.calculateIntegral,segment,1,"Метод Трапеций")
